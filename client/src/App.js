d20ddc7eb3bee4eced899970100c87b4a9883557
Rohit Shrivastava <rohit.shrivastava@tadigital.com>
2024-01-10 17:29:17 +0530

    Added access key

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,19 @@ import Layout from "./components/Layout";
 import Home2 from "./Home2";
 import Home3 from "./Home3";
 
-function App() {
+function App(props) {
+  const accessKey = new URLSearchParams(window.location.search).get('key');
  return (
    <div className="App">
      <BrowserRouter>
        <Routes>
          <Route element={<Layout />} >
-           <Route path="/" element={<Home />} />
-           <Route path="edit" element={<Edit />} />
+
+           <Route exact path="/" element={accessKey == process.env.REACT_APP_LOCAL_ACCESS_KEY ? (<Home />) : (<Contact />)} />
+           <Route path="edit" element={accessKey == process.env.REACT_APP_LOCAL_ACCESS_KEY ? (<Edit />) : (<Contact />)} />
            <Route path="contact" element={<Contact />} />
-           <Route path="home2" element={<Home2 />} />
-           <Route path="home3" element={<Home3 />} />
+           <Route path="home2" element={accessKey == process.env.REACT_APP_LOCAL_ACCESS_KEY ? (<Home2 />) : (<Contact />)} />
+           <Route path="home3" element={accessKey == process.env.REACT_APP_LOCAL_ACCESS_KEY ? (<Home3 />) : (<Contact />)} />
          </Route>
        </Routes>
      </BrowserRouter>
