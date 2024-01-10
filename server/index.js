8e2c96d3143001e126e55e56b9c5c92c6ab0793f
Rohit Shrivastava <rohit.shrivastava@tadigital.com>
2024-01-10 14:59:24 +0530

    Fixed node path issue

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Server is running.');
 });
 
