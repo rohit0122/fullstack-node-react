fb57014a540783075c5809642c7273bade8e96fd
Rohit Shrivastava <rohit.shrivastava@tadigital.com>
2024-01-10 17:01:57 +0530

    added z-index to loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,7 +2,7 @@ import Spinner from 'react-bootstrap/Spinner';
 
 const Loader = (props) => {
     return (
-        props.show && <div className="position-fixed d-flex justify-content-center align-items-center w-100 h-100 top-0 bg-dark opacity-75">
+        props.show && <div style={{"zIndex": 99999}} className="position-fixed d-flex justify-content-center align-items-center w-100 h-100 top-0 bg-dark opacity-75">
         <Spinner animation="border" role="status" variant='danger'>
           <span className="visually-hidden">Loading...</span>
         </Spinner>
